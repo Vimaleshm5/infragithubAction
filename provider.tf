58f8bf01a1c035d52311fb305314560bc0906f5e
vimaleshm5 <vimaleshm6@gmail.com>
2025-09-16 20:46:22 +0530

    resource group

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.43.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "8b10287d-12d6-41e3-b62c-33457c006e96"
+}
+
+
+
+
+resource "azurerm_resource_group" "gitgroup" {
+    name = "gitgroup"
+    location = "east asia"
+  
+}
